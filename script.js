074acc54f6cb913c0bc67c30b123dabb0d2970dd
Trifle22 <xromchenkov99@gmail.com>
2021-02-04 15:46:09 +0300

    areYouLucky closures hard

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 'use strict';
 
 
-function guessTheNumber() {
-  let n = prompt('Угадай число от 1 до 100 :)');
+function guessTheNumber(num) {
 
-  function areYouLucky(num) {
+  function areYouLucky(num, attempts) {
+    console.log(num);
+    let n = prompt('Угадай целое число от 1 до 100 :)');
     if (parseInt(n) > num) {
-      alert('Загаданное число меньше');
-      guessTheNumber();
+      attempts--;
+      if (attempts === 0) {
+        confirm('Попытки закончились, хотите сыграть еще?');
+      } else {
+        alert('Загаданное число меньше, осталось попыток ' + attempts);
+        areYouLucky(num, attempts);
+      }
     } else if (parseInt(n) < num) {
-      alert('Загаданное число больше');
-      guessTheNumber();
-    } else if (n === '' || isNaN(n)) {
+      attempts--;
+      if (attempts === 0) {
+        if (confirm('Попытки закончились, хотите сыграть еще?')) {
+          areYouLucky(Math.floor(Math.random() * 101),10);
+        }
+      } else {
+        alert('Загаданное число больше, осталось попыток ' + attempts);
+        areYouLucky(num, attempts);
+      }
+    } else if (n.trim() === '' || isNaN(n)) {
       alert('Введи число!');
-      guessTheNumber();
+      areYouLucky(num, attempts);
     } else if (n === null) {
       confirm('Игра окончена');
     } else if (Number(n) === num) {
       alert('Поздравляю, Вы выиграли! <3');
+      if (confirm('Попытки закончились, хотите сыграть еще?')) {
+        areYouLucky(Math.floor(Math.random() * 101),10);
+      }
     }
   }
 
-  areYouLucky(22);
+  areYouLucky(num, 10);
 }
 
-guessTheNumber();
+guessTheNumber(Math.floor(Math.random() * 101));
 
