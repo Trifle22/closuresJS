4ed7601ad8c5a9eac647a55c6803dad09d56e80f
Trifle22 <xromchenkov99@gmail.com>
2021-02-04 14:45:53 +0300

    areYouLucky closures

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log(1);
+'use strict';
+
+
+function guessTheNumber() {
+  let n = prompt('Угадай число от 1 до 100 :)');
+
+  function areYouLucky(num) {
+    if (parseInt(n) > num) {
+      alert('Загаданное число меньше');
+      guessTheNumber();
+    } else if (parseInt(n) < num) {
+      alert('Загаданное число больше');
+      guessTheNumber();
+    } else if (n === '' || isNaN(n)) {
+      alert('Введи число!');
+      guessTheNumber();
+    } else if (n === null) {
+      confirm('Игра окончена');
+    } else if (Number(n) === num) {
+      alert('Поздравляю, Вы выиграли! <3');
+    }
+  }
+
+  areYouLucky(22);
+}
+
+guessTheNumber();
+
